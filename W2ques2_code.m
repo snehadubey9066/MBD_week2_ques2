1c4ec4d1942f1361dde9d3eac2c29a2b4e144da4
snehadubey9066 <59720911+snehadubey9066@users.noreply.github.com>
2020-12-13 10:26:56 +0530

    Added code to run and open simulation with plot and data inspector

---
+++ W2ques2_code.m
@@ -0,0 +1,10 @@
+%% code to open and run the simulation
+
+sim("W2ques2_system.slx")
+open_system("W2ques2_system")
+
+% code to open scope
+open_system("W2ques2_system/Scope")
+
+% code to open data inspector
+Simulink.sdi.view
